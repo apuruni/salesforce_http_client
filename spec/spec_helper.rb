bb862684bdac652510489d1a8e104557b6284368
apuruni <apuruni@gmail.com>
2015-01-25 23:19:41 +0900

    add coveralls to spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,9 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'salesforce_http_client'
 
+require 'coveralls'
+Coveralls.wear!
+
 RSpec.configure do
   require 'webmock/rspec'
 end
